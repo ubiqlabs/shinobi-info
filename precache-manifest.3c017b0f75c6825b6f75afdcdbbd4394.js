00c0e8a35681eba22842dab5fee2a1841ea47d35
Jaimish Patel <jaimish.patel95@gmail.com>
2021-11-30 15:35:22 -0500

    1.0.0

---
+++ precache-manifest.3c017b0f75c6825b6f75afdcdbbd4394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c47473c081d0bab8ff3d31deb8b18e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4368c19f6a1f6481ffe",
+    "url": "/static/js/2.2c10033a.chunk.js"
+  },
+  {
+    "revision": "7ca68a7c9b7e1c3837a2e609b6814187",
+    "url": "/static/js/2.2c10033a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae78e7443d3f5f20d343",
+    "url": "/static/js/main.f10cbf52.chunk.js"
+  },
+  {
+    "revision": "a92dd80c9da5e49a112f",
+    "url": "/static/js/runtime-main.090c699a.js"
+  },
+  {
+    "revision": "f23378cef8d5ac55bbd2a3c6b59dc194",
+    "url": "/static/media/wordmark_white.f23378ce.svg"
+  }
+]);
