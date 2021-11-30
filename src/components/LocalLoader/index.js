cb7333bf51e687dad86423ac7bde78d706fdee56
Jaimish Patel <jaimish.patel95@gmail.com>
2021-11-30 13:56:56 -0500

    Use loader

--- src/components/LocalLoader/index.js
+++ src/components/LocalLoader/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import styled, { css, keyframes } from 'styled-components'
-import { useDarkModeManager } from '../../contexts/LocalStorage'
 
-const pulse = keyframes`
-  0% { transform: scale(1); }
-  60% { transform: scale(1.1); }
-  100% { transform: scale(1); }
+const rotate = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
 `
 
 const Wrapper = styled.div`
@@ -27,19 +25,34 @@ const Wrapper = styled.div`
 `
 
 const AnimatedImg = styled.div`
-  animation: ${pulse} 800ms linear infinite;
+  animation: ${rotate} 1.1s cubic-bezier(0.795, 0.155, 0.210, 0.880) infinite;
   & > * {
     width: 72px;
   }
 `
 
-const LocalLoader = ({ fill }) => {
-  const [darkMode] = useDarkModeManager()
+const StyledSVG = styled(`svg`, { size: String, fill: String, stroke: String })`
+  width: 32px;
+  height: 32px;
+  display: flex;
+  path {
+    stroke: ${({ stroke, theme }) => stroke ?? theme.primary1};
+    fill: ${({ fill, theme }) => fill ?? theme.bg6};
+  }
+`
 
+const LocalLoader = ({ fill, stroke, ...rest }) => {
   return (
     <Wrapper fill={fill}>
       <AnimatedImg>
-        <img src={require(darkMode ? '../../assets/logo_white.svg' : '../../assets/logo.svg')} alt="loading-icon" />
+        <StyledSVG viewBox="0 0 24 24" fill={null} xmlns="http://www.w3.org/2000/svg" stroke={stroke} {...rest}>
+          <path
+            d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 9.27455 20.9097 6.80375 19.1414 5"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </StyledSVG>
       </AnimatedImg>
     </Wrapper>
   )
